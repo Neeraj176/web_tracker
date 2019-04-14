f9c926a1b51f2ee870cfc2e0b2fdb5cb10f437fa
Neeraj176 <neerajkamra76@gmail.com>
2019-04-14 23:53:50 +0530

    Add files via upload

---
+++ web-customer-tracker/src/org/mywork/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package org.mywork.springdemo.service;
+
+import java.util.List;
+
+import org.mywork.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
